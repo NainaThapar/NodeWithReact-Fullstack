bf5ade7f5ab3cd268a304a3889e96e5f14f5d60b
Naina Thapar <Naina@Nainas-MacBook-Pro-3.local>
2019-01-14 22:55:29 +0530

    google authentication page

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./server/config/keys");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-}); //route handler
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there" });
+// }); //route handler
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 const PORT = process.env.PORT || 5000; //to read port from heroku(underlying deployment)
 app.listen(PORT);
