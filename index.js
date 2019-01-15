d703b5e3ae411d20380f7e6c9dcb9d1e0b06217f
Naina Thapar <Naina@Nainas-MacBook-Pro-3.local>
2019-01-15 23:18:58 +0530

    refactor

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./server/config/keys");
+require("./server/services/passport");
+require("./server/routes/authRoutes");
 
 const app = express();
+
+require("./server/routes/authRoutes")(app);
 
 // app.get("/", (req, res) => {
 //   res.send({ hi: "there" });
 // }); //route handler
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
 
 const PORT = process.env.PORT || 5000; //to read port from heroku(underlying deployment)
 app.listen(PORT);
