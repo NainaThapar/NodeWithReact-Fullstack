0acc71941536a996b2bf28f19940c0813d34ffa8
Naina Thapar <Naina@Nainas-MacBook-Pro-3.local>
2019-01-27 23:07:41 +0530

    redux reducers and routing

--- server/services/passport.js
+++ server/services/passport.js
@@ -22,23 +22,21 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       //console.log("access token : ", accessToken);
       //console.log("refresh token : ", refreshToken);
       //console.log("profile ", profile);
 
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        //async func
-        if (existingUser) {
-          //we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          //we don't have a record with this ID. Make a new record.
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user)); //save method saves it to db else it's just an instance in js
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+      //async func
+      if (existingUser) {
+        //we already have a record with the given profile ID
+        done(null, existingUser);
+      } else {
+        //we don't have a record with this ID. Make a new record.
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user); //save method saves it to db else it's just an instance in js
+      }
     }
   )
 );
